3759a57d7b91ab608eaf632b6b360cf10c5d23b1
Garik Nurmurodoff <garikn95@gmail.com>
2018-04-14 23:27:58 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');   
+
+var app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html'); // set up ejs for templating
+
+app.use('/custom', express.static(__dirname + '/custom')); 				// set the static files location /public/img will be /img for users
+app.use('/scripts', express.static(__dirname + '/node_modules')); 				// set the static files location /public/img will be /img for users
+
+
+require('./app/routes.js')(app);
+
+app.listen(process.env.PORT || 8888, function (){
+	console.log('port - 8888');
+});
